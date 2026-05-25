777ec4b761f7f2d319a93bf6011cbac5c158d143
Shwetank-Maurya <sd3564086@gmail.com>
2026-05-25 17:06:50 +0530

    Added routers to complete create API

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 
 const{ PORT }=require('./config/serverConfig');
-const CityRepository = require('.repository/city-repository');
+const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async () => {
 
@@ -13,10 +13,10 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     
+    app.use('/api',ApiRoutes);
+
     app.listen(PORT, () => {
         console.log(`Server Started at ${PORT}.`);
-        const repo = new CityRepository();
-        repo.createCity({name:"New Delhi"});
     });
 }
 
