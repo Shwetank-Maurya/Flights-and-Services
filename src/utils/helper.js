05320335de83f95f253faaf8d945d60cdc284714
Shwetank-Maurya <sd3564086@gmail.com>
2026-05-27 17:48:48 +0530

    Added flights create crud

---
+++ src/utils/helper.js
@@ -0,0 +1,10 @@
+function compareTime(timeString1, timeString2) {
+    let dateTime1 = new Date(timeString1);
+    let dataTime2 = new Date(timeString2);
+
+    return dateTime1.getTime() > dateTime2.getTime();
+}
+
+module.exports = {
+    compareTime,
+}
